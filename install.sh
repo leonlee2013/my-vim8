073e4cec716f4cf5a2273add2ba96de6d26400db
leon <leonlee2013@gmail.com>
2022-07-24 01:24:46 +0000

    修改安装脚本

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
  #git clone https://github.com/tpope/vim-pathogen.git
- cd ./.vim/bundle
- for github in `cat ./vim_plugins.ini` ; do
-   git clone $github
- done
+
+ set -e
+ ABSFile=`realpath $0`
+ ExecPath=`realpath "$(dirname $ABSFile)"`
+ mkdir -p $ExecPath/.vim/bundle && cd $ExecPath/.vim/bundle
+  for github in `cat $ExecPath/vim_plugins.ini` ; do
+    git clone $github
+  done
 

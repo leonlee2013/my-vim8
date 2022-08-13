cfaaf77824613d26e25c799fa272250fb713e309
leon <leonlee2013@gmail.com>
2022-08-13 18:12:05 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
  #git clone https://github.com/tpope/vim-pathogen.git
 
- #set -e
+ set -e
  ABSFile=`realpath $0`
  ExecPath=`realpath "$(dirname $ABSFile)"`
  mkdir -p $ExecPath/.vim/bundle && cd $ExecPath/.vim/bundle

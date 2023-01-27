279d121835e07614811f360087399af1fdec7042
leon <leonlee2013@gmail.com>
2023-01-27 17:32:33 +0800

    add apt install

--- ycm_install_for_debian11.sh
+++ ycm_install_for_debian11.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install -y cmake python3-devel npm
+sudo apt-get install -y cmake npm
 cd ~/.vim/bundle/
 git clone https://github.com/ycm-core/YouCompleteMe
 cd ./YouCompleteMe

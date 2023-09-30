bf04c4ab9dba20ab36ee7549ce92146925910f74
liyong <leonlee2013@gmail.com>
2023-09-30 22:21:57 +0800

    更新install

--- install.sh
+++ install.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 #git clone https://github.com/tpope/vim-pathogen.git
 
- set -e
- ABSFile=`realpath $0`
- ExecPath=`realpath "$(dirname $ABSFile)"`
- mkdir -p $ExecPath/.vim/bundle && cd $ExecPath/.vim/bundle
-  for github in `cat $ExecPath/vim_plugins.ini` ; do
-    git clone $github
-  done
- cd $ExecPath
+set -e
+ABSFile=`realpath $0`
+ExecPath=`realpath "$(dirname $ABSFile)"`
+mkdir -p $ExecPath/.vim/bundle && cd $ExecPath/.vim/bundle
+# for github in `cat $ExecPath/vim_plugins.ini` ; do
+# git clone $github
+# done
+cd $ExecPath
 
 # 获取当前日期和时间，并格式化为YYYY-MM-DD_HH-MM-SS的形式
-current_date=$(date +"%Y-%m-%d_%H-%M-%S")
+CurrentDate=$(date +"%Y-%m-%d_%H-%M-%S")
+VimrcFPath=$(realpath ~/.vimrc)
+VimDPath=$(realpath ~/.vim)
 # 使用日期来修改文件名
-  #备份.vimrc
- if [ -f "~/.vimrc" ]; then
-  mv "~/.vimrc" "~/.vimrc_${current_date}"
-  echo "已备份为：~/.vimrc_${current_date}"
- else
-  echo ".vimrc不存在"
- fi
+#备份.vimrc
+if [ -f $VimrcFPath ]; then
+  mv $VimrcFPath ${VimrcFPath}_${CurrentDate}
+  echo "已备份为：${VimrcFPath}_${CurrentDate}"
+else
+  echo ".vimrc不存在, 不需要备份"
+fi
 
- #备份.vim目录
- if [ -d "~/.vimrc" ]; then
-  mv "~/.vim" "~/.vim_${current_date}"
-  echo "已备份为：~/.vim_${current_date}"
- else
-  echo ".vim目录不存在"
- fi
- cp -rf ./.vimrc ~/
- cp -rf ./.vim ~/
+#备份.vim目录
+if [ -d $VimDPath ]; then
+  mv $VimDPath "${VimDPath}_${CurrentDate}"
+  echo "已备份为：${VimDPath}_${CurrentDate}"
+else
+  echo ".vim目录不存在, 不需要备份"
+fi
+cp -rf ./.vimrc ~/
+cp -rf ./.vim ~/

de6905ef9be04e4cf3716e037d11c624395ef1a9
liyong <leonlee2013@gmail.com>
2023-10-01 21:51:48 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ git clone $github
 done
 
 #用elixir-ls 0.14.6, 支持elixir1.12.3
-cd ~/.vim/bundle/vim-lsp-settings/ && git checkout 63d1d31
+cd $ExecPath/.vim/bundle/vim-lsp-settings/ && git checkout 63d1d31
 cd $ExecPath
 
 # 获取当前日期和时间，并格式化为YYYY-MM-DD_HH-MM-SS的形式

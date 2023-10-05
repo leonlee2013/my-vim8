005da39dc7a708f89d512d25fba6734fed16b8f6
liyong <leonlee2013@gmail.com>
2023-10-05 15:48:45 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,22 +5,22 @@ set -e
 ABSFile=`realpath $0`
 ExecPath=`realpath "$(dirname $ABSFile)"`
 mkdir -p $ExecPath/.vim/bundle && cd $ExecPath/.vim/bundle
-#for github in `cat $ExecPath/vim_plugins.ini` ; do
-#git clone $github
-#done
+for github in `cat $ExecPath/vim_plugins.ini` ; do
+ git clone $github
+done
 
 #用elixir-ls 0.14.6, 支持elixir1.12.3
-#cd $ExecPath/.vim/bundle/vim-lsp-settings/ && git checkout 63d1d31
+cd $ExecPath/.vim/bundle/vim-lsp-settings/ && git checkout 63d1d31
 cd $ExecPath
 
 # 获取当前日期和时间，并格式化为YYYY-MM-DD_HH-MM-SS的形式
 CurrentDate=$(date +"%Y-%m-%d_%H-%M-%S")
 #兼容mac
-VimrcFPath=$(realpath -q ~/.vimrc | echo "")
-VimDPath=$(realpath -q ~/.vim | echo "")
+VimrcFPath=$(realpath -q ~/.vimrc || echo "")
+VimDPath=$(realpath -q ~/.vim || echo "")
 # 使用日期来修改文件名
 #备份.vimrc
-if [ "$VimrcFPathX" != "X" ] && [ -f $VimrcFPath ]; then
+if [ "${VimrcFPath}X" != "X" ] && [ -f $VimrcFPath ]; then
   echo "已备份为：${VimrcFPath}_${CurrentDate}"
   mv $VimrcFPath ${VimrcFPath}_${CurrentDate}
 else
@@ -28,7 +28,7 @@ else
 fi
 
 #备份.vim目录
-if [ "$VimDPathX" != "X" ] && [ -d $VimDPath ]; then
+if [ "${VimDPath}X" != "X" ] && [ -d $VimDPath ]; then
   mv $VimDPath "${VimDPath}_${CurrentDate}"
   echo "已备份为：${VimDPath}_${CurrentDate}"
 else
